8b610be6be86f14b2b6de191b553ed4ccade5a5d
im24230679 <L24230679@minatitlan.tecnm.mx>
2024-11-11 19:47:32 -0600

    Update Problema 10 .py

--- Problema 10 .py
+++ Problema 10 .py
@@ -1,1 +1,20 @@
+# 10) En un curso de computación, que consta de 24 alumnos, 
+# deberán armar un algoritmo que informe por pantalla el apellido y nombre junto a la nota de examen de cada alumno.
 
+num_alumnos = 24
+alumnos = []
+alumnos_numeros = [i for i in range(num_alumnos)]
+
+i = 0
+while i < num_alumnos:
+    print(f"Alumno {alumnos_numeros[i] + 1}:")
+    apellido = input("Ingrese el apellido: ")
+    nombre = input("Ingrese el nombre: ")
+    nota = float(input("Ingrese la nota de examen: "))
+    alumnos.append((apellido, nombre, nota))
+    i += 1
+    print()
+
+print("Información de los alumnos:")
+for alumno in alumnos:
+    print(f"{alumno[0]} {alumno[1]} - Nota de examen: {alumno[2]}")
