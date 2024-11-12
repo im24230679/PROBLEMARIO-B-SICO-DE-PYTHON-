93402e47234f3b81978b3f35ebb1f85c2c3697bc
im24230679 <L24230679@minatitlan.tecnm.mx>
2024-11-11 19:43:07 -0600

    Update Problema 9 .py

--- Problema 9 .py
+++ Problema 9 .py
@@ -1,1 +1,13 @@
+# 9) Ingresar un valor por teclado y determinar 
+# si es positivo, negativo o igual a cero, 
+# imprimir una leyenda en cada caso
 
+
+numero = float(input("Ingresa un número: "))
+
+if numero > 0:
+    print("El número es positivo.")
+elif numero < 0:
+    print("El número es negativo.")
+else:
+    print("El número es cero.")
