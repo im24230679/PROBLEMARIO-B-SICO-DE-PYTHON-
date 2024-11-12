ce4d9a6c0cd87013d4fbc30e6542a700f7c9a035
im24230679 <L24230679@minatitlan.tecnm.mx>
2024-11-11 19:53:59 -0600

    Update Problema 12 .py

--- Problema 12 .py
+++ Problema 12 .py
@@ -1,1 +1,9 @@
+# 12) Ingresar valores por teclado y acumularlos en una variable detener el proceso cuando la suma supere los 78500, al final imprimir el resultado
 
+suma = 0
+
+while suma <= 78500:
+    valor = float(input("Ingresa un valor: "))
+    suma += valor
+
+print(f"La suma total es: {suma}")
