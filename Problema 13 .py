ce8e6f5b425a7fc821281aa41cf680eb49df4418
im24230679 <L24230679@minatitlan.tecnm.mx>
2024-11-11 19:55:58 -0600

    Update Problema 13 .py

--- Problema 13 .py
+++ Problema 13 .py
@@ -1,1 +1,9 @@
+# 13) Ingresar 10 valores por teclado y obtener la sumatoria de los mismos. Imprimir los resultados.
 
+suma = 0
+
+while suma <= 78500:
+    valor = float(input("Ingresa un valor: "))
+    suma += valor
+
+print(f"La suma total es: {suma}")
